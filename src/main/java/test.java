715510c2ad3e5cf42db97c1b12315be42d271b7b
TolstobrowNikita <nikita.tolstobrow23@mail.ru>
2023-09-06 23:06:43 +0300

    z1_2_6

---
+++ src/main/java/test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main(String[] args) {
+        z1_2_6 nazvanie = new z1_2_6();
+        System.out.println(nazvanie.drawisMonitorsCounter(17,5));
+    }
+}
---
+++ src/main/java/z1_2_6.java
@@ -0,0 +1,7 @@
+public class z1_2_6 {
+    public static void main(String[] args) {
+    }
+    public int drawisMonitorsCounter(int monitors, int programmers) {
+        return monitors % programmers;
+    }
+}

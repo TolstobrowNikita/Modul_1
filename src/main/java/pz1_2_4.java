f9aed69bb65f6216e85ce965fab1d0e1b6463c23
TolstobrowNikita <nikita.tolstobrow23@mail.ru>
2023-09-06 22:39:49 +0300

    z1_2_4

---
+++ src/main/java/pz1_2_4.java
@@ -0,0 +1,12 @@
+public class pz1_2_4 {
+    public static void main(String[] args) {
+        boolean varBoolean = false;
+        byte varByte = 5;
+        short varShort = 3;
+        int varInt = 7;
+        long varLong = 4;
+        double varDouble = 8.5;
+        float varFloat = 9.4f;
+        char varChar = 2;
+    }
+}

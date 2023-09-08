8a19a0f5ef3cf55efc75915a2f8b22232d880df0
Никита <nikita-vip1904@yandex.ru>
2023-09-08 17:05:29 +0300

    1. Добавил в игнор файлы .idea что бы не отображались в коммите 2. Чекнул все задания до 1_3_2

--- src/main/java/z1_3_2.java
+++ src/main/java/z1_3_2.java
@@ -1,6 +1,21 @@
 public class z1_3_2 {
+    public static void main(String[] args) {
+        System.out.println(charExpression(22));
+        System.out.println(charExpressionNew(22));
+    }
+
+    /**
+     * В целом правильно, но можно было решить в 1 строчку :)
+     */
     public static char charExpression(int a) {
         char c = '\\';
         return (char) (((int) c) + a);
+    }
+
+    /**
+     * Решение в 1 строчку
+     */
+    public static char charExpressionNew(int a) {
+        return (char) ('\\' + a);
     }
 }

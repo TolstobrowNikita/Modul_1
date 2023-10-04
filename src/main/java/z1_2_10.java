cc624da9efac8b0ac8115153fbafc9fef5f4437f
TolstobrowNikita <nikita.tolstobrow23@mail.ru>
2023-10-04 21:26:19 +0300

    add z1_2_10 and 1_3_3

--- src/main/java/z1_2_10.java
+++ src/main/java/z1_2_10.java
@@ -1,5 +1,9 @@
 public class z1_2_10 {
     public static boolean doubleExpression(double a, double b, double c) {
-        return Math.abs(a + b) == c;
+        boolean WoW = false;
+        if (Math.abs((a + b) - c) <= 1E-4) {
+            WoW = true;
+        }
+        return WoW;
     }
 }
--- src/main/java/z1_3_3.java
+++ src/main/java/z1_3_3.java
@@ -1,5 +1,8 @@
 public class z1_3_3 {
+    public static void main(String[] args) {
+        System.out.println(getAgeDiff((byte) 15, (byte) 22));
+    }
     public static byte getAgeDiff(byte age1, byte age2) {
-        return (byte) (age1 - age2);
-        }
+        return (byte) Math.abs(age1 - age2);
+    }
 }

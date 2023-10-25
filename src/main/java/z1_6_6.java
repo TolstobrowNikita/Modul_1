dcf3531a8f0959242f0d06173fc0c802a2e98f67
TolstobrowNikita <nikita.tolstobrow23@mail.ru>
2023-10-25 21:11:59 +0300

    add z1_6_6 and 1_6_8

---
+++ src/main/java/z1_6_6.java
@@ -0,0 +1,6 @@
+public class z1_6_6 {
+    public static void main(String[] args) {
+        String name = "Nikita";
+        System.out.println(name);
+    }
+}
---
+++ src/main/java/z1_6_8.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class z1_6_8 {
+    public void parseAndPrintNumber(String str) {
+        System.out.println(Integer.parseInt(str) / 2);
+    }
+}
+
+
